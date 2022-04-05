8d1624d7fca21c2b8a3c06e94eaba5c44373f21a
lgorniak <lukasgorniak@gmail.com>
2022-04-05 17:16:35 +0200

    add an example of embedding expressions

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Counter() {
+	const [count, setCounter] = useState(0);
+
+	const formatCounter = () => {
+		return count === 0 ? "Zero" : count;
+	};
+
 	return (
 		<React.Fragment>
-			<h1>Hello World</h1>
-			<button>Increment</button>
+			<h1>{formatCounter()}</h1>
+			<button onClick={() => setCounter(count + 1)}>Increment</button>
 		</React.Fragment>
 	);
 }
