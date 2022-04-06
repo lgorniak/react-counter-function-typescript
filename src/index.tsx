1451ab63c7ecf78d1e7317163febd1dd42c17989
lgorniak <lukasgorniak@gmail.com>
2022-04-06 02:45:10 +0200

    change to new root render

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,11 @@ import "./index.css";
 import Counter from "./components/counter";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<Counter />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container);
+root.render(<Counter />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
