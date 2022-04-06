41483d00c3e7b8f24776f5a20ab14002d5f33713
lgorniak <lukasgorniak@gmail.com>
2022-04-06 08:52:43 +0200

    refactor-methods

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Count } from "../types/count";
 
-function Counter() {
+const formatCounter = (counter: Count | null): string | number => {
+	let counterValue: string | number;
+
+	counter
+		? counter.number === 0
+			? (counterValue = `Zero`)
+			: (counterValue = counter.number)
+		: (counterValue = `State Object not set`);
+
+	return counterValue;
+};
+
+const iterateCounter = (
+	counter: Count | null,
+	customHook: React.Dispatch<React.SetStateAction<Count | null>>
+) => {
+	if (counter) customHook({ number: counter.number + 1 });
+};
+
+const Counter = (): JSX.Element => {
 	const [counter, setCounter] = useState<Count | null>(null);
 
-	useEffect(() => {
+	useEffect((): void => {
 		setCounter({ number: 0 });
 	}, []);
 
-	const formatCounter = () => {
-		let counterValue: string | number;
-
-		counter
-			? counter.number === 0
-				? (counterValue = `Zero`)
-				: (counterValue = counter.number)
-			: (counterValue = `State Object not set`);
-
-		return counterValue;
-	};
-
-	const iterateCounter = () => {
-		if (counter) setCounter({ number: counter.number + 1 });
-	};
-
 	return (
 		<>
-			<h1>{formatCounter()}</h1>
-			<button onClick={iterateCounter}>Increment</button>
+			<h1>{formatCounter(counter)}</h1>
+			<button onClick={() => iterateCounter(counter, setCounter)}>
+				Increment
+			</button>
 		</>
 	);
-}
+};
 
 export default Counter;
